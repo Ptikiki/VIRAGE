57b044a287a89aa34a5b9d23600892bb4ac0b8bd
Ptikiki <alicia.baudry.31@gmail.com>
2017-03-14 17:37:21 +0100

    trace ta ligne

--- scripts/oeil.js
+++ scripts/oeil.js
@@ -5,13 +5,16 @@ window.onload = function() {
 /*** LISTENERS ***/
 window.addEventListener('resize', handleResize)
 window.addEventListener('keydown',  keyDetected)
+window.addEventListener('mousedown',  onMouseDown)
+window.addEventListener('mouseup',  onMouseUp)
 
 
 /*** SCENE SETUP ***/
 let renderer,
     stage
 let nbr_yeux = 30,
-    circles = []
+    circles = [],
+    dessinPoint
 
 /*** CANVAS DRAWING ***/
 function initCanvas() {
@@ -70,4 +73,46 @@ function animate() {
   for (var i = 0; i < circles.length; i++) {
     circles[i].x += 0.5
   }
+}
+
+function onMouseDown(event) {
+/*  renderer.clearBeforeRender = false
+  renderer.preserveDrawingBuffer = true*/
+  let mouseX = event.offsetX;
+  let mouseY = event.offsetY;
+  dessinPoint = new PIXI.Graphics();
+  dessinPoint.beginFill(0xffffff);
+  dessinPoint.lineStyle(10, 0xffffff);
+  dessinPoint.moveTo(mouseX, mouseY);
+
+  stage.addChild(dessinPoint);
+  window.addEventListener('mousemove',  onMouseDrag)
+}
+
+function onMouseDrag(event) {
+  let mouseX = event.offsetX;
+  let mouseY = event.offsetY;
+    dessinPoint.lineTo(mouseX+50, mouseY+50);
+    dessinPoint.lineTo(mouseX+50, mouseY+50);
+    dessinPoint.lineTo(mouseX+50, mouseY+50);
+
+      dessinPoint.endFill();
+
+}
+
+function onMouseUp(event) {
+
+/*  renderer.clearBeforeRender = true
+  renderer.preserveDrawingBuffer = false*/
+  window.removeEventListener('mousemove',  onMouseDrag)
+}
+
+function drawWhatYouWant(x, y) {
+  let trait = new PIXI.Graphics();
+  trait.beginFill(0xffffff);
+  trait.drawCircle(0, 0, 1);
+  trait.endFill();
+  trait.x = mouseX;
+  trait.y = mouseY;
+  stage.addChild(trait);
 }
