8bc4f7037c16e1d6d92026d1e4d38bbc006a5760
MelinaGuyon <guyonmelina@gmail.com>
2017-03-16 15:29:25 +0100

    Start positioning drawing sections

--- scripts/datas.js
+++ scripts/datas.js
@@ -1,38 +1,73 @@
 const datas = {
   datasArdoises: [
     {
-      width: 300,
+      width: 500,
       height: 250,
-      x: 150,
-      y: 250
+      x: 300,
+      y: 350
     }, {
-      width: 300,
-      height: 250,
-      x: 650,
-      y: 400
+      width: 950,
+      height: 300,
+      x: 1550,
+      y: 200
+    }, {
+      width: 600,
+      height: 500,
+      x: 6250,
+      y: 1080
     }
   ],
   datasCheckPoints: [
     {
-      rayon: 10,
-      x: 170,
-      y: 290,
-      ardoise: 0
-    }, {
-      rayon: 10,
-      x: 290,
-      y: 290,
-      ardoise: 0
-    }, {
-      rayon: 10,
-      x: 700,
-      y: 450,
-      ardoise: 1
-    }, {
-      rayon: 10,
-      x: 750,
+      rayon: 30,
+      x: 340,
       y: 490,
+      ardoise: 0
+    }, {
+      rayon: 30,
+      x: 540,
+      y: 540,
+      ardoise: 0
+    }, {
+      rayon: 30,
+      x: 750,
+      y: 460,
+      ardoise: 0
+    }, {
+      rayon: 30,
+      x: 1590,
+      y: 320,
       ardoise: 1
+    }, {
+      rayon: 30,
+      x: 2000,
+      y: 430,
+      ardoise: 1
+    }, {
+      rayon: 30,
+      x: 2450,
+      y: 290,
+      ardoise: 1
+    }, {
+      rayon: 30,
+      x: 6320,
+      y: 1280,
+      ardoise: 2
+    }, {
+      rayon: 30,
+      x: 6650,
+      y: 1320,
+      ardoise: 2
+    }, {
+      rayon: 30,
+      x: 6650,
+      y: 1580,
+      ardoise: 2
+    }, {
+      rayon: 30,
+      x: 6250,
+      y: 1080,
+      ardoise: 2
     }
   ]
 }
